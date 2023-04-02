8b82334209c4c0b07877a46468ebfb4e271412e7
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-03 02:02:28 +0300

    Add image field to API

--- movies/models.py
+++ movies/models.py
@@ -6,6 +6,7 @@ class Moviedata(models.Model):
     duration = models.FloatField()
     ratings = models.FloatField()
     type_movie = models.CharField(max_length=100,default='action')
+    image = models.ImageField(upload_to='images/',default='images/none/noimage.jpg')
     
     def __str__(self):
         return self.name
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from movies.views import MovieViewSet, ActionViewSet, AnimationViewSet
+from django.conf.urls.static import static
+from django.conf import settings
 
 router = routers.SimpleRouter()
 router.register('movies', MovieViewSet)
@@ -26,4 +28,4 @@ router.register('animations', AnimationViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
