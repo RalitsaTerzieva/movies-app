9b263bf9fdc0424163c51c1f5257b2bf9514f035
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-03 01:35:15 +0300

    Add type movie in models

--- movies/models.py
+++ movies/models.py
@@ -5,6 +5,7 @@ class Moviedata(models.Model):
     name = models.CharField(max_length=200)
     duration = models.FloatField()
     ratings = models.FloatField()
+    type_movie = models.CharField(max_length=100,default='action')
     
     def __str__(self):
         return self.name
